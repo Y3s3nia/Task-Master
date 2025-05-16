4de770975342867e152289668b34023a0defbebe
Y3s3nia <y3s3nian3ri@gmail.com>
2025-05-16 01:25:31 -0500

    cleaned up unneeded code.

--- Task-Master/src/Navbars.jsx
+++ Task-Master/src/Navbars.jsx
@@ -1,25 +1,8 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
-import Contact from './Contact';
-import ToDo from './ToDo';
-
 
 function Navbars () {
-    function Todo () {
-        return (
-            <div>
-                <h1>To Do List</h1>
-                {/* <ToDo /> */}
-            </div>
-        );
-    }
-    
-    function Contacts () {
-        return (<h1>Contact</h1>);
-    }
-
     function NavbarTask() {
         return (
             <Navbar expand="lg" className="bg-body-tertiary fixed-top" bg="dark" data-bs-theme="dark">
